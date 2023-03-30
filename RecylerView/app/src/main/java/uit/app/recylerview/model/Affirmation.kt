ada28674ba67623fd8b10660e424299e78944ba0
HungDangQuang <hungdangkg@gmail.com>
2023-03-30 10:16:03 +0700

    exercise

--- RecylerView/app/src/main/java/uit/app/recylerview/model/Affirmation.kt
+++
@@ -1,9 +0,0 @@
-package uit.app.recylerview.model
-
-import androidx.annotation.DrawableRes
-import androidx.annotation.StringRes
-
-data class Affirmation(
-    @StringRes val stringResourcesId: Int,
-    @DrawableRes val imageResourceId: Int) {
-}
